3bc9e92a73aba71550c48a6295652ae5f059cf4b
Anderson Meira <anderson.meira@gmail.com>
2017-02-19 23:51:57 -0300

    alteração na configuração do heroku

--- models/index.js
+++ models/index.js
@@ -8,11 +8,24 @@ var env       = process.env.NODE_ENV || 'development';
 var config    = require(__dirname + '/../config/config.json')[env];
 var db        = {};
 
-if (config.use_env_variable) {
-  var sequelize = new Sequelize(process.env[config.use_env_variable]);
+
+if (process.env.HEROKU_POSTGRESQL_CRIMSON_URL) {
+    // the application is executed on Heroku ... use the postgres database
+    sequelize = new Sequelize(process.env.HEROKU_POSTGRESQL_CRIMSON_URL, {
+      dialect:  'postgres',
+      protocol: 'postgres',
+      port:     match[4],
+      host:     match[3],
+      logging:  true //false
+    })
 } else {
-  var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  if (config.use_env_variable) {
+    var sequelize = new Sequelize(process.env[config.use_env_variable]);
+  } else {
+    var sequelize = new Sequelize(config.database, config.username, config.password, config);
+  }  
 }
+
 
 fs
   .readdirSync(__dirname)
